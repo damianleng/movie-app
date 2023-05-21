27f34c7e002afd1cb0d01f8a7fb8c3f6d045dc96
damianlengdy <2021185leng@aupp.edu.kh>
2023-05-21 09:28:10 +0700

    Initial commit

---
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieService } from 'src/app/services/movie.service';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-movie-details',
+  templateUrl: './movie-details.page.html',
+  styleUrls: ['./movie-details.page.scss'],
+})
+export class MovieDetailsPage implements OnInit {
+  movie: any = null;
+  imageBaseUrl = environment.images;
+  constructor(private route: ActivatedRoute, private movieService: MovieService) { }
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    if(id !== null){
+      this.movieService.getMovieDeatils(id).subscribe((res)=>{
+        console.log(res);
+        this.movie = res;
+      })
+    }
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '95c0922fbffff182a7fe828f1de87fed',
+  baseUrl: 'https://api.themoviedb.org/3',
+  images: 'http://image.tmdb.org/t/p',
 };
 
 /*
